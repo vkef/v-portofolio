07639df6813570cc494bfcdb6e5b4b48bb009ace
Vasileios Kefalas <kef.vasileios@outlook.com>
2020-04-18 20:59:16 +0300

    Menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,7 +5,7 @@ const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 
-const navItems = document.querySelectorAll("nav-item");
+const navItems = document.querySelectorAll(".nav-item");
 
 //Set Initial State of Menu
 let showMenu = false;
